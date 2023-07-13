960bcecc87ef010d1069034a39d1a98347f51b1c
Smipe-a <smipe.artem@gmail.com>
2023-07-13 16:29:27 +0600

    Refactoring the file

--- scripts/main_table_results.py
+++ scripts/main_table_results.py
@@ -2,38 +2,38 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
-# Send GET-request on web-site
-url = "https://www.skysports.com/premier-league-table/2022"
-response = requests.get(url)
-soup = BeautifulSoup(response.content, "html.parser")
 
-# Find main table on site
-table = soup.find("table", class_="standing-table__table")
+def parse_main_table(url: str) -> pd.DataFrame:
+    # Send GET-request on web-site
+    soup = BeautifulSoup(requests.get(url).content, 'html.parser')
+    # Find main table on site
+    table = soup.find('table', class_='standing-table__table')
 
-# Create a list to receive data from page
-main_table_results = []
-
-# Iterate through each row of the table
-for row in table.find_all("tr"):
-    # Retrieve the value of a cell
-    cells = row.find_all("td")
-    # Create a dictionary with the parsed data
-    if len(cells) > 0:
-        row_data = {
-            "position": cells[0].text.strip(),
-            "team": cells[1].find("a", class_="standing-table__cell--name-link").text.strip(),
-            "played": cells[2].text.strip(),
-            "won": cells[3].text.strip(),
-            "drawn": cells[4].text.strip(),
-            "lost": cells[5].text.strip(),
-            "goals_for": cells[6].text.strip(),
-            "goals_against": cells[7].text.strip(),
-            "goals_difference": cells[8].text.strip(),
-            "points": cells[9].text.strip()
+    # Create a list to receive data from page
+    main_table_results = [
+        {
+            'position': cells[0].get_text(strip=True),
+            'team': cells[1].find('a', class_='standing-table__cell--name-link').get_text(strip=True),
+            'played': cells[2].get_text(strip=True),
+            'won': cells[3].get_text(strip=True),
+            'drawn': cells[4].get_text(strip=True),
+            'lost': cells[5].get_text(strip=True),
+            'goals_for': cells[6].get_text(strip=True),
+            'goals_against': cells[7].get_text(strip=True),
+            'goals_difference': cells[8].get_text(strip=True),
+            'points': cells[9].get_text(strip=True)
         }
-        main_table_results.append(row_data)
+        # Iterate through each row of the table
+        for row in table.find_all('tr')
+        # Retrieve the value of a cell
+        # Create a dictionary with the parsed data
+        if len(cells := row.find_all('td')) > 0
+    ]
 
-data_main_table_results = pd.DataFrame(main_table_results)
-data_main_table_results = data_main_table_results.set_index('position')
-data_main_table_results.to_csv('main_table_results.csv')
+    return pd.DataFrame(main_table_results).set_index('position')
 
+
+if __name__ == '__main__':
+    source_url = 'https://www.skysports.com/premier-league-table/2022'
+    data_main_table_results = parse_main_table(source_url)
+    data_main_table_results.to_csv('../data/main_table_results.csv')
