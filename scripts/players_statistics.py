9664852a32c324cd2c6d071c3b75dcc3570dbc1e
Smipe-a <smipe.artem@gmail.com>
2023-07-15 21:35:43 +0600

    Script for data parsing has been completed, and the data is being supplemented from a new source

--- scripts/players_statistics.py
+++ scripts/players_statistics.py
@@ -16,7 +16,81 @@ players_statistics_away_team = []
 match_officials = []
 
 
-def get_statistics(match_id, players, is_home, is_first_team):
+def quantity_events(event):
+    # We count the number of occurrences of a specific event
+    count_events = 0
+    for _ in event:
+        count_events += 1
+    return count_events
+
+
+def format_minutes_to_int(minute) -> int:
+    minute = re.sub(r'[^\d+]', '', minute)
+    if minute.find('+') == -1:
+        return int(minute)
+    else:
+        index_plus = minute.find('+')
+        return int(minute[:index_plus]) + int(minute[index_plus + 1:])
+
+
+def format_age(age: str) -> int:
+    return int(re.sub(r'\D', '', age))
+
+
+def format_position(position: str) -> str:
+    if position == 'Goalkeeper':
+        return 'GK'
+    abbr_position = position.replace('-', ' ').split(' ')
+    if len(abbr_position) < 2:
+        return abbr_position[0][0].upper() + abbr_position[0][1:]
+    return abbr_position[0][0] + abbr_position[1][0]
+
+
+def format_price(price: str):
+    coefficient = 1
+    if price[-1] == 'm':
+        coefficient = 1e6
+    elif price[-1] == 'k':
+        coefficient = 1e3
+
+    match = re.search(r'[\d.]+', price)
+    if match:
+        return float(match.group()) * coefficient
+    return None
+
+
+def get_additional_info(url, player_skysports, dict_players, block_players):
+    sub_block_players = block_players.find_all('table', class_='inline-table')
+    i = 0
+    for player in sub_block_players:
+        player_transfermarket = block_players.select('td.zentriert.rueckennummer')[i].find(
+            'div', class_='rn_nummer').text.strip()
+
+        # Fix two number players
+        if url == f'https://www.transfermarkt.com/manchester-city_fulham-fc/aufstellung/spielbericht/3838230' and \
+                format_price(player.find('tr').find_next_sibling().text.strip()) == 1000000.0 and \
+                format_age(player.find('tr').text.strip()) == 17:
+            player_transfermarket = '82'
+
+        if url == 'https://www.transfermarkt.com/leicester-city_brentford-fc/aufstellung/spielbericht/3837818' and \
+                player_transfermarket == '12':
+            player_transfermarket = '1'
+        if url == 'https://www.transfermarkt.com/leicester-city_brentford-fc/aufstellung/spielbericht/3837818' and \
+                player_transfermarket == '29' and \
+                format_price(player.find('tr').find_next_sibling().text.strip()) == 20000000.0:
+            player_transfermarket = '20'
+
+        if str(player_skysports) == player_transfermarket:
+            nationality = player.find_next('td', class_='zentriert').find('img').get('title')
+            age = format_age(player.find('tr').text.strip())
+            position = format_position(block_players.select('td.zentriert.rueckennummer')[i].get('title'))
+            transfer_fee = format_price(player.find('tr').find_next_sibling().text.strip())
+
+            dict_players[player_transfermarket] = [nationality, age, position, transfer_fee]
+        i += 1
+
+
+def get_statistics(url, players, soup_transfermarket, is_home, is_first_team):
     for player in players:
         number = player.find_previous('dt', class_='sdc-site-team-lineup__player-number').text.strip()
         # Find the first name and last name of the football player,
@@ -31,59 +105,84 @@ def get_statistics(match_id, players, is_home, is_first_team):
         else:
             full_name = f'{name} {surname}'
 
-        start_minute, substitution_minute = 0, 0
+        if is_first_team:
+            play_time = 90
+        else:
+            play_time = 0
         goals, penalty_goals, missed_penalty_goals, own_goals, assists = 0, 0, 0, 0, 0
         substitution, yellow_card, red_card = False, False, False
 
         event_list = player.find_all(class_='sdc-site-team-lineup__events')
         try:
-            for events in event_list:
+            for event in event_list:
                 if is_first_team:
-                    if events.find_all(class_='sdc-site-team-lineup__item-event-icon--sub-off'):
-                        # event_sub_off = events.find_all(class_='sdc-site-team-lineup__item-event-icon--sub-off')
-                        # event_off = event_sub_off.find_next(class_="sdc-site-team-lineup__event_time").text.strip()
-                        # print()
-                        # print(event_off)
+                    if event.find(class_='sdc-site-team-lineup__item-event-icon--sub-off'):
+                        sub_off = event.find_all('li', class_='sdc-site-team-lineup__event')[-1]
+                        sub_off_time = sub_off.find('span', class_='sdc-site-team-lineup__visually-hidden').text.strip()
                         substitution = True
-                        # substitution_minute = format_minutes_to_int(event_off[0])
+                        play_time = format_minutes_to_int(sub_off_time)
                 else:
-
-                    if events.find_all(class_='sdc-site-team-lineup__item-event-icon--sub-on'):
+                    if event.find(class_='sdc-site-team-lineup__item-event-icon--sub-on'):
+                        sub_on = event.find_all('li', class_='sdc-site-team-lineup__event')[0]
+                        sub_on_time = sub_on.find('span', class_='sdc-site-team-lineup__visually-hidden').text.strip()
                         substitution = True
-                        # event_sub_on = events.find_all(class_='sdc-site-team-lineup__item-event-icon--sub-on')
-                        # event_on = event_sub_on.find_next(class_="sdc-site-team-lineup__event_time").text.strip()
-                        # print()
-                        # print(event_on)
-                        # start_minute = format_minutes_to_int(event_on[0])
-                        # event_sub_off = events.find_all(class_='sdc-site-team-lineup__item-event-icon--sub-off')
-                        # if event_sub_off:
-                        #     substitution_minute = format_minutes_to_int(event_sub_off[0])
+                        play_time = 90 - format_minutes_to_int(sub_on_time)
+                    if event.find(class_='sdc-site-team-lineup__item-event-icon--sub-off'):
+                        sub_off = event.find_all('li', class_='sdc-site-team-lineup__event')[1]
+                        sub_off_time = sub_off.find('span', class_='sdc-site-team-lineup__visually-hidden').text.strip()
+                        play_time = 90 - format_minutes_to_int(sub_off_time) - play_time
 
-                goals = quantity_events(events.find_all(class_='sdc-site-team-lineup__item-event-icon--goal'))
+                # Counting the number of events that have occurred
+                goals = quantity_events(event.find_all(class_='sdc-site-team-lineup__item-event-icon--goal'))
                 penalty_goals = quantity_events(
-                    events.find_all(class_='sdc-site-team-lineup__item-event-icon--penalty'))
+                    event.find_all(class_='sdc-site-team-lineup__item-event-icon--penalty'))
                 missed_penalty_goals = quantity_events(
-                    events.find_all(class_='sdc-site-team-lineup__item-event-icon--penalty-miss'))
-                own_goals = quantity_events(events.find_all(class_='sdc-site-team-lineup__item-event-icon--own-goal'))
-                assists = quantity_events(events.find_all(class_='sdc-site-team-lineup__item-event-icon--assist'))
+                    event.find_all(class_='sdc-site-team-lineup__item-event-icon--penalty-miss'))
+                own_goals = quantity_events(event.find_all(class_='sdc-site-team-lineup__item-event-icon--own-goal'))
+                assists = quantity_events(event.find_all(class_='sdc-site-team-lineup__item-event-icon--assist'))
 
-                if events.find_all(class_='sdc-site-team-lineup__item-event-icon--yellow-card'):
+                if event.find_all(class_='sdc-site-team-lineup__item-event-icon--yellow-card'):
                     yellow_card = True
-                if events.find_all(class_='sdc-site-team-lineup__item-event-icon--red-card'):
+                if event.find_all(class_='sdc-site-team-lineup__item-event-icon--red-card'):
                     red_card = True
         except AttributeError:
-            start_minute, substitution_minute = 0, 0
+            if is_first_team:
+                play_time = 90
+            else:
+                play_time = 0
             goals, penalty_goals, missed_penalty_goals, own_goals, assists = 0, 0, 0, 0, 0
             substitution, yellow_card, red_card = False, False, False
 
+        # Supplement the information from transfermarket.com
+        # with transfer fee, age, nationality, and position at the time of the match
+        all_home_players = soup_transfermarket.find_all('div', class_='responsive-table')
+        # Create a dictionary where the key is the player's number,
+        # and the value is a list with a dimension of 4
+        dict_info_players = {}
+
+        if is_home:
+            get_additional_info(url, number, dict_info_players, all_home_players[0])
+            get_additional_info(url, number, dict_info_players, all_home_players[2])
+        else:
+            get_additional_info(url, number, dict_info_players, all_home_players[1])
+            get_additional_info(url, number, dict_info_players, all_home_players[3])
+
+        # print(dict_info_players)
+
+        print(number, full_name, dict_info_players[str(number)][0], dict_info_players[str(number)][1],
+              dict_info_players[str(number)][2], dict_info_players[str(number)][3], is_first_team)
+
         player_statistics = {
-            'match_id': match_id,
+            'match_id': int(url[-7:]),
             'number_player': number,
             'name_player': full_name,
+            'nationality': dict_info_players[str(number)][0],
+            'age': dict_info_players[str(number)][1],
+            'position': dict_info_players[str(number)][2],
+            'transfer_fee': dict_info_players[str(number)][3],
             'first_team': is_first_team,
             'substitution': substitution,
-            'start_minute': start_minute,
-            'substitution_minute': substitution_minute,
+            'play_time': play_time,
             'goals': goals,
             'penalty_goals': penalty_goals,
             'missed_penalty_goals': missed_penalty_goals,
@@ -92,29 +191,13 @@ def get_statistics(match_id, players, is_home, is_first_team):
             'yellow_card': yellow_card,
             'red_card': red_card
         }
+        # print(player_statistics)
 
         # Check if the player played for the away team or the home team
         if is_home:
             players_statistics_home_team.append(player_statistics)
         else:
             players_statistics_away_team.append(player_statistics)
-
-
-def quantity_events(event):
-    # We count the number of occurrences of a specific event
-    count_events = 0
-    for _ in event:
-        count_events += 1
-    return count_events
-
-
-def format_minutes_to_int(minute):
-    minute = re.sub(r'[^\d+]', '', minute)
-    if minute.find('+') == -1:
-        return int(minute)
-    else:
-        index_plus = minute.find('+')
-        return int(minute[:index_plus]) + int(minute[index_plus + 1:])
 
 
 def set_officials_match(match_id: int, name_officials: str, role: str):
@@ -126,13 +209,15 @@ def set_officials_match(match_id: int, name_officials: str, role: str):
     match_officials.append(official)
 
 
+# Parsing referee crew data from the skysports website
+# and adding missing coach information for each match from the transfermarket website
 def get_officials_match(source_url, soup_skysports, soup_transfermarket):
     match_id = int(source_url[-7:])
+
     manager_block = soup_transfermarket.find_all('div', class_='row sb-formation')[-1]
     manager_name_block = manager_block.find_all('table', class_='inline-table')
     manager_home_name = manager_name_block[0].find('img').get('title')
     manager_away_name = manager_name_block[1].find('img').get('title')
-    print(manager_home_name, manager_away_name)
 
     set_officials_match(match_id, manager_home_name, 'Manager Home Team')
     set_officials_match(match_id, manager_away_name, 'Manager Away Team')
@@ -153,54 +238,50 @@ def scrape_data():
         for url in file:
             print(i)
             i += 1
+
             response = requests.get(url.strip(), timeout=20)
             soup_ss = BeautifulSoup(response.text, 'html.parser')
 
-            transfermarkt_match_url = dict_match_all_url[url[35:-14]]
-            print(transfermarkt_match_url)
-            response = requests.get(transfermarkt_match_url, headers={'User-Agent': UserAgent().chrome}, timeout=50)
+            transfermarkt_match_sheet = dict_match_all_url[url[35:-14]]
+            response = requests.get(transfermarkt_match_sheet, headers={'User-Agent': UserAgent().chrome}, timeout=50)
+            print(transfermarkt_match_sheet)
+
+            # Generating a link to navigate to the page with match lineups
+            url_lineup = BeautifulSoup(response.text, 'html.parser').find('a', string='Line-ups').get('href')
+            transfermarket_line_ups = f'https://www.transfermarkt.com{url_lineup}'
+
+            response = requests.get(transfermarket_line_ups, headers={'User-Agent': UserAgent().chrome}, timeout=20)
             soup_tm = BeautifulSoup(response.text, 'html.parser')
 
-            get_url_lineup = soup_tm.find('a', string='Line-ups').get('href')
-            url_lineup = f'https://www.transfermarkt.com{get_url_lineup}'
-            response = requests.get(url_lineup, headers={'User-Agent': UserAgent().chrome}, timeout=20)
-            soup_tm = BeautifulSoup(response.text, 'html.parser')
-
-
+            # Creating the match_officials table
             get_officials_match(url, soup_ss, soup_tm)
 
-
-
-
-            block_team = soup_ss.find_all('div', class_='sdc-site-team-lineup__header sdc-site-team-lineup__header--main')
+            # Creating the players_statistics tables
+            block_team = soup_ss.find_all('div', class_='sdc-site-team-lineup__header '
+                                                        'sdc-site-team-lineup__header--main')
             flag_team = True
-
             for team in block_team:
                 main_players = team.find_next_sibling('dl').find_all('dd', class_='sdc-site-team-lineup__player-name')
-                get_statistics(int(url[-7:]), main_players, flag_team, True)
+                get_statistics(transfermarket_line_ups, main_players, soup_tm, flag_team, True)
 
                 substitutes = team.find_next_sibling('h5', class_='sdc-site-team-lineup__header--subs')
                 if substitutes:
                     substitutes_players = substitutes.find_next_sibling('dl').find_all(
                         'dd', class_='sdc-site-team-lineup__player-name')
-                    get_statistics(int(url[-7:]), substitutes_players, flag_team, False)
+                    get_statistics(transfermarket_line_ups, substitutes_players, soup_tm, flag_team, False)
                 flag_team = False
-
-
-        # print(match_officials)
 
 
 if __name__ == '__main__':
     scrape_data()
-    # data_players_statistics_home_team = pd.DataFrame(players_statistics_home_team)
-    # data_players_statistics_away_team = pd.DataFrame(players_statistics_away_team)
     data_match_officials = pd.DataFrame(match_officials)
-
-    # data_players_statistics_home_team = data_players_statistics_home_team.set_index('match_id')
-    # data_players_statistics_home_team.to_csv('../data/players_statistics_home_team.csv')
-    #
-    # data_players_statistics_away_team = data_players_statistics_away_team.set_index('match_id')
-    # data_players_statistics_away_team.to_csv('../data/players_statistics_away_team.csv')
-
     data_match_officials = data_match_officials.set_index('match_id')
     data_match_officials.to_csv('../data/match_officials.csv')
+
+    data_players_statistics_home_team = pd.DataFrame(players_statistics_home_team)
+    data_players_statistics_home_team = data_players_statistics_home_team.set_index('match_id')
+    data_players_statistics_home_team.to_csv('../data/players_statistics_home_team.csv')
+
+    data_players_statistics_away_team = pd.DataFrame(players_statistics_away_team)
+    data_players_statistics_away_team = data_players_statistics_away_team.set_index('match_id')
+    data_players_statistics_away_team.to_csv('../data/players_statistics_away_team.csv')
